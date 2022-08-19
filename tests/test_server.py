58bcc424d32ebe4f38146b9fa0014f52791dcfa2
Dmytro <dmytro.bezruk@sourcex.net>
2022-08-19 15:26:05 +0300

    Added pytest.ini

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,4 +3,4 @@ from starlette import status
 
 def test_main_url(client) -> None:
     response = client.get('/')
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_200_OK, "Status must be 200"
